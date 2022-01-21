cfbba01bf77b84b06599a6a59974551437e4fafc
Michele Krueger <michele.krueger@etermax.de>
2022-01-21 20:51:15 +0100

    Add test with cubes that scale away

---
+++ Assets/Scripts/Camera/HideShader.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using DG.Tweening;
+using UnityEngine;
+
+public class HideShader : MonoBehaviour
+{
+
+    [SerializeField] private float hideDistance = 10f;
+    
+    private void Awake()
+    {
+    }
+
+    void Update()
+    {
+        var scale = 1f - Mathf.Clamp01(hideDistance - UnityEngine.Vector3.Distance(Camera.main.transform.position, transform.position));
+        transform.DOScale(scale, 0.1f);
+        transform.RotateAround(UnityEngine.Vector3.zero, UnityEngine.Vector3.down, Time.deltaTime * 10f);
+    }
+
+}
