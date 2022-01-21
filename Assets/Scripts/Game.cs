5d7fe9efb37fac0d392b329fae9618fb46162a87
Michele Krueger <michele.krueger@etermax.de>
2022-01-21 20:25:48 +0100

    Add game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,12 +1,24 @@
-﻿using DG.Tweening;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using DG.Tweening;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    [SerializeField] private Button test;
+    
     // Start is called before the first frame update
     void Start()
     {
-        transform.DOMove(Vector3.back, 1f);
+        transform.DOMove(UnityEngine.Vector3.back, 1f);
+        test.OnClickAsObservable().Subscribe(OnClick);
     }
 
+    private void OnClick(Unit unit)
+    {
+        throw new System.NotImplementedException();
+    }
 }
