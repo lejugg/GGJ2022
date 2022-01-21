f8dae8f77d0c410c1c724f7b9e826fe21f71e97f
Michele Krueger <michele.krueger@etermax.de>
2022-01-21 20:10:36 +0100

    add dotween.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMove(Vector3.back, 1f);
+    }
+
+}
